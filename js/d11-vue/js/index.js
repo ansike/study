bcf5d85d390ac9309d072db9d27c0645ecbc198c
ansike <984315765@qq.com>
2018-11-20 09:15:22 +0800

    js 基础

--- js/d11-vue/js/index.js
+++ js/d11-vue/js/index.js
@@ -5,35 +5,35 @@
  * @param {*} options 
  */
 
-function SelfVue (options) {
-    var self = this;
-    this.data = options.data;
-    this.methods = options.methods;
-    //将data上的所有属性代理到this上
-    Object.keys(this.data).forEach(function(key) {
-        self.proxyKeys(key);
-    });
-    //-----------------------beforeCreate
-    //劫持数据
-    observe(this.data);
-    //处理dom。绑定事件。挂载dom
-    new Compile(options.el, this);
+function SelfVue(options) {
+  var self = this;
+  this.data = options.data;
+  this.methods = options.methods;
+  //将data上的所有属性代理到this上
+  Object.keys(this.data).forEach(function (key) {
+    self.proxyKeys(key);
+  });
+  //-----------------------beforeCreate
+  //劫持数据
+  observe(this.data);
+  //处理dom。绑定事件。挂载dom
+  new Compile(options.el, this);
 
-    options.mounted.call(this); // 所有事情处理好后执行mounted函数
+  options.mounted.call(this); // 所有事情处理好后执行mounted函数
 }
 
 SelfVue.prototype = {
-    proxyKeys: function (key) {
-        var self = this;
-        Object.defineProperty(this, key, {
-            enumerable: false,
-            configurable: true,
-            get: function getter () {
-                return self.data[key];
-            },
-            set: function setter (newVal) {
-                self.data[key] = newVal;
-            }
-        });
-    }
+  proxyKeys: function (key) {
+    var self = this;
+    Object.defineProperty(this, key, {
+      enumerable: false,
+      configurable: true,
+      get: function getter() {
+        return self.data[key];
+      },
+      set: function setter(newVal) {
+        self.data[key] = newVal;
+      }
+    });
+  }
 }
