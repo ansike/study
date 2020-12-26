7d5c861d551740b862bb29d6696e04dc4ef1d316
安思科 <ansike@bytedance.com>
2020-12-26 11:49:22 +0800

    feat: svg

--- js/d45-ts/src/utility.ts
+++ js/d45-ts/src/utility.ts
@@ -20,6 +20,9 @@ type readonlyPageInfo = Readonly<PageInfo>;
 // Partial<Type>
 type partialPageInfo = Partial<PageInfo>;
 
+// Required<Type>
+type requiredPageInfo = Required<PageInfo>;
+
 // Pick<Type, Keys>
 type pickPageInfo = Pick<PageInfo, 'name' | 'id'>
 
@@ -36,3 +39,62 @@ type excludePageInfo = Exclude<Page, 'home' | 'about'>;
 type nonNullablePage = NonNullable<Page | null>;
 
 // Parameters<Type>
+declare function fn(a: string, b: number): { a: number; b: string };
+type parametersType1 = Parameters<typeof fn>;
+type parametersType2 = Parameters<() => string>;
+type parametersType3 = Parameters<(a: string, b: boolean) => string>;
+
+// ConstructorParameters<Type>
+type constructorParameters1 = ConstructorParameters<ErrorConstructor>
+type constructorParameters2 = ConstructorParameters<FunctionConstructor>
+type constructorParameters3 = ConstructorParameters<RegExpConstructor>
+
+// ReturnType<Type>
+type returnType = ReturnType<typeof fn>
+
+// InstanceType<Type>
+class CClass {
+  x = 0;
+  y = 0;
+}
+type instanceType = InstanceType<typeof CClass>
+
+// ThisParameterType<Type>
+function toHex(this: Number) {
+  return this.toString(16);
+}
+
+function numberToString(n: ThisParameterType<typeof toHex>) {
+  return toHex.apply(n);
+}
+
+// OmitThisParameter<Type>
+const fiveToHex: OmitThisParameter<typeof toHex> = toHex.bind(5);
+
+console.log(fiveToHex());
+
+// ThisType<Type>
+type ObjectDescriptor<D, M> = {
+  data?: D;
+  methods?: M & ThisType<D & M>; // Type of 'this' in methods is D & M
+};
+
+function makeObject<D, M>(desc: ObjectDescriptor<D, M>): D & M {
+  let data: object = desc.data || {};
+  let methods: object = desc.methods || {};
+  return { ...data, ...methods } as D & M;
+}
+
+let obj = makeObject({
+  data: { x: 0, y: 0 },
+  methods: {
+    moveBy(dx: number, dy: number) {
+      this.x += dx; // Strongly typed this
+      this.y += dy; // Strongly typed this
+    },
+  },
+});
+
+obj.x = 10;
+obj.y = 20;
+obj.moveBy(5, 5);
