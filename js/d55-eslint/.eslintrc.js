d9c37a7f800da49fdb8488b271f0f98e8e2b8af8
安思科 <ansike@bytedance.com>
2021-01-09 23:26:08 +0800

    fix; 提交4

--- js/d55-eslint/.eslintrc.js
+++ js/d55-eslint/.eslintrc.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 module.exports = {
   parser: '@typescript-eslint/parser', //定义ESLint的解析器
   extends: [
--- js/d55-eslint/src/b.ts
+++ js/d55-eslint/src/b.ts
@@ -3,6 +3,3 @@ console.log(a);
 const asda = 111 + a;
 
 console.log(asda);
-const asd = {
-  as: 'as',
-};
