f5f1ac88aeb5fb38d37f629412074fea620864fd
ansike <984315765@qq.com>
2021-01-25 09:26:40 +0800

    npm 传参

--- js/d23-npm/npm-command/index.js
+++ js/d23-npm/npm-command/index.js
@@ -1,3 +1,11 @@
 #! /usr/bin/env node
-
+const path = require("path");
+const fs = require("fs");
+console.log(process.env.npm_config_argv);
+const paths = JSON.parse(process.env.npm_config_argv).remain;
+console.log(paths[0]);
+const file = path.join(paths[0], "push.js");
+console.log(file);
+const res = fs.readFileSync(file, 'utf-8');
+console.log(res);
 console.log("test-command");
