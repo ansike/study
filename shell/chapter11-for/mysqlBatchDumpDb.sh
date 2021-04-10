27817432bca59c2f093c9d9eda7fc517a092d81f
安思科 <ansike@bytedance.com>
2021-04-10 12:40:32 +0800

    feat: 批量操作数据

--- shell/chapter11-for/mysqlBatchDumpDb.sh
+++ shell/chapter11-for/mysqlBatchDumpDb.sh
@@ -5,14 +5,22 @@ PASSWORD=123456
 LOGINCMD="mysql -u$USER -p$PASSWORD"
 DUMPCMD="mysqldump -u$USER -p$PASSWORD"
 
-[ ! -d $DBPATH ] && {
-  mkdir -p $DBPATH
-}
 # 指定拷贝
 # for dbname in oldBody oldgirl xxx yyy
 
 # 获取mysql中的所需要数据库转存
-for dbname in `${LOGINCMD} -e "show databases;" | sed '1' | egrep -v "mysql|schema"`
+# for dbname in `${LOGINCMD} -e "show databases;" | sed '1d' | egrep -v "mysql|schema"`
+# do
+#   $DUMPCMD $dbname | gzip > $DBPATH/${dbname}_$(date +%F)_sql.gz
+# done
+
+rm -rf $DBPATH
+for dbname in `${LOGINCMD} -e "show databases;" | sed '1d' | egrep -v "mysql|schema"`
 do
-  $DUMPCMD $dbname | gzip > $DBPATH/${dbname}_$(date +%F)_sql.gz
+  mkdir -p $DBPATH/${dbname}
+  for table in `${LOGINCMD} -e "use ${dbname}; show tables;" | sed '1d'`
+  do
+    $DUMPCMD $dbname $table | gzip > $DBPATH/${dbname}/${table}_$(date +%F)_sql.gz
+  done
 done
+
