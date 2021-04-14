bf9634e2b8f8ec8577373ad2080fa7283fc13b71
ansike <ansike@haha.com>
2021-04-14 09:38:17 +0800

    feat: 数组匹配

--- git/command.sh
+++ git/command.sh
@@ -5,4 +5,5 @@ git config --global alias.br branch
 git config --global alias.cf config
 git config --global alias.fc fetch
 git config --global alias.aa "add ."
+git config --global alias.cm "commit -m"
 
--- shell/chapter13-array/array.sh
+++ shell/chapter13-array/array.sh
@@ -24,3 +24,28 @@ echo ${array[1]}
 
 # 数组长度
 echo ${#array}
+
+# 截取数组
+echo ${array[*]}
+# 截取位置到结尾
+echo ${array[*]:1}
+# 截取位置及长度
+echo ${array[*]:1:1}
+
+# 删除数组
+array[4]=one4one
+# 从头开始 one change test one4one
+echo ${array[*]} 
+echo ${array[*]#one} #change test 4one
+echo ${array[*]##one*} # change test
+# 从尾开始 one change test one4one
+echo ${array[*]}
+echo ${array[*]%one} # change test one4
+echo ${array[*]%%one*} # change test
+
+# 替换数组
+echo ${array[*]}
+# 仅第一次匹配到替换
+echo ${array[*]/one/one1}
+# 匹配到所有替换
+echo ${array[*]//one/one1}
