d44d9fc08cd82e150a9d9f1b78c2274662497999
ansike <984315765@qq.com>
2021-04-14 09:21:11 +0800

    feat: array

---
+++ shell/chapter13-array/array.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+###
+ # @Description: description
+ # @Author: Ask
+ # @LastEditors: Ask
+ # @Date: 2021-04-13 09:05:36
+ # @LastEditTime: 2021-04-14 09:21:04
+### 
+array=(one two three)
+# array2=($(ls ../))
+array2=(`ls ../`)
+array3=([1]=one [2]=two [3]=three)
+array4[1]=one; array4[2]=two; array4[3]=three;
+echo ${array[*]}
+echo ${array[@]}
+echo ${array2[@]}
+echo ${array3[@]}
+echo ${array4[@]}
+
+# 增
+array[3]=test
+echo ${array[*]}
+# 删
+unset array[2]
+echo ${array[*]}
+# 改
+array[1]=change
+echo ${array[*]}
+# 查
+echo ${array[1]}
+
+# 数组长度
+echo ${#array}
