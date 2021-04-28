2e5abd3de016f688653c1ad289255296ecd58fe5
ansike <984315765@qq.com>
2021-04-28 09:33:14 +0800

    feat: webpack5

---
+++ webpack/demo/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: {
+    print: path.resolve(__dirname, "./src/print.js"),
+    app: path.resolve(__dirname, "./src/index.js"),
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output",
+      template: path.resolve(__dirname, "./index.html"),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg|gif|ico)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/,
+        use: ["xml-loader"],
+      },
+    ],
+  },
+};
