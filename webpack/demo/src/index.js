683a54c46433edb966fc917b90c1507de8691fe6
安思科 <ansike@bytedance.com>
2021-05-10 13:56:01 +0800

    feat: hot

--- webpack/demo/src/index.js
+++ webpack/demo/src/index.js
@@ -1,15 +1,31 @@
+import './style.css';
+import { cube } from './math.js';
+import printMe from './print.js';
+
 async function getComponent() {
-  try {
-    const { default: _ } = await import("lodash");
-    const element = document.createElement("div");
-    element.innerHTML = _.join(["Hello", "webpack"], " ");
-    return element;
-  } catch (error) {
-    console.log("An error occurred while loading the component");
-    return false;
-  }
+	try {
+		const { default: _ } = await import('lodash');
+		const element = document.createElement('div');
+		// element.innerHTML = _.join([ 'Hello', 'webpack21' ], ' ');
+    element.innerHTML = [
+      'Hello webpack！1',
+      '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
+		return element;
+	} catch (error) {
+		console.log('An error occurred while loading the component');
+		return false;
+	}
 }
 (async () => {
-  const component = await getComponent();
-  if (component) document.querySelector("#app").appendChild(component);
+	const component = await getComponent();
+	if (component) document.querySelector('#app').appendChild(component);
 })();
+
+// TODO 作用
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!');
+    printMe('Updating print.js...');
+  })
+}
--- webpack/demo/webpack.config.js
+++ webpack/demo/webpack.config.js
@@ -6,6 +6,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
   devServer: {
     contentBase: "./dist",
+    hot: true,
   },
   mode: process.env.NODE_ENV,
   // mode: "production",
